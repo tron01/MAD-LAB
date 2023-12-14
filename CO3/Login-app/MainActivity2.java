b0c9fa465e04ce3a2acd6c7ed162e91c10506816
tron01 <69385258+tron01@users.noreply.github.com>
2023-12-14 17:37:48 +0530

    login app

---
+++ CO3/Login-app/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.app6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+    }
+
+
+
+}
